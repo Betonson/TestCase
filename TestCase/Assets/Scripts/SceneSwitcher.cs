3f5b389437a1e0c8271677907ff1e0f61fb3da63
Betonson <Leonel3289@mail.ru>
2021-05-14 00:03:38 +0300

    UI creation
    Created basic UI for pause menu, added some animations, good progress overall

---
+++ TestCase/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public GameObject restartPanel;
+    public GameObject pauseButton;
+    public void GoToGameScene()
+    {
+        SceneManager.LoadScene("TinyPlanets");
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+    public void ContinueButton()
+    {
+        restartPanel.SetActive(false);
+        //pauseButton.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    public void GoToMainMenuButton()
+    {
+        //SceneManager.LoadScene("Main menu");
+        Debug.Log("Went to main menu");
+    }
+
+    public void PauseButton()
+    {
+        restartPanel.SetActive(true);
+        //pauseButton.SetActive(false);
+        Time.timeScale = 0;
+    }
+}
